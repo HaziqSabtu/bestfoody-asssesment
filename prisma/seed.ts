95d9e3c393c3cebaaae509b9268cde659f5a78ae
haziqsabtu <mhaziq.sabtu@gmail.com>
2025-04-19 23:23:13 +0800

    db: update seeded data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,80 +1,64 @@
 import { PrismaClient } from '@prisma/client';
 
+//mockData
+import { restaurants as restaurantsMock } from './mockData/restaurants';
+import { users as usersMock } from './mockData/users';
+import { reviews as reviewsMock } from './mockData/reviews';
+import { RestaurantCategoryType } from 'src/modules/restaurant/entities/restaurant.entity';
+
 const prisma = new PrismaClient();
 
 async function main() {
   // Clean current database
   await prisma.review.deleteMany({});
-  await prisma.restaurant.deleteMany({});
   await prisma.restaurantRating.deleteMany({});
   await prisma.restaurantImage.deleteMany({});
+  await prisma.restaurant.deleteMany({});
   await prisma.user.deleteMany({});
 
   // Create Users
-  const alice = await prisma.user.create({
-    data: {
-      email: 'alice@example.com',
-      firstName: 'Alice',
-      lastName: 'Smith',
-      password: 'password123',
-    },
+  const users = await prisma.user.createManyAndReturn({
+    data: usersMock,
   });
 
-  const bob = await prisma.user.create({
-    data: {
-      email: 'bob@example.com',
-      firstName: 'Bob',
-      lastName: 'Smith',
-      password: 'password123',
-    },
-  });
+  const firstTenUsers = users.slice(0, 10);
 
-  // Create Restaurants
-  const pizzaPlace = await prisma.restaurant.create({
-    data: {
-      name: 'Pizza Palace',
-      category: 'ITALIAN',
-      userId: alice.id,
-    },
-  });
-
-  const sushiSpot = await prisma.restaurant.create({
-    data: {
-      name: 'Sushi Central',
-      category: 'JAPANESE',
-      userId: bob.id,
-    },
+  const restaurants = await prisma.restaurant.createManyAndReturn({
+    data: restaurantsMock.map((restaurant) => {
+      const randomUser = firstTenUsers[Math.floor(Math.random() * 10)];
+      return {
+        ...restaurant,
+        category: restaurant.category as RestaurantCategoryType,
+        userId: randomUser.id,
+      };
+    }),
   });
 
   // Add Reviews
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const reviews = await prisma.review.createMany({
-    data: [
-      {
-        userId: alice.id,
-        restaurantId: pizzaPlace.id,
-        rating: 4,
-        reviewText: 'Delicious crust!',
-      },
-      {
-        userId: bob.id,
-        restaurantId: pizzaPlace.id,
-        rating: 5,
-        reviewText: 'Best pizza in town!',
-      },
-      {
-        userId: alice.id,
-        restaurantId: sushiSpot.id,
-        rating: 3,
-        reviewText: 'Good but pricey.',
-      },
-    ],
-  });
+  for (const restaurant of restaurants) {
+    // get random length from total reviews
+    const randomReviewsLength = Math.floor(Math.random() * reviewsMock.length);
 
-  // Compute and insert average ratings manually
-  const restaurants = [pizzaPlace.id, sushiSpot.id];
+    const shuffledReviews = [...reviewsMock]
+      .sort(() => Math.random() - 0.5)
+      .splice(0, randomReviewsLength);
 
-  for (const restaurantId of restaurants) {
+    const shuffledUsers = [...users]
+      .sort(() => Math.random() - 0.5)
+      .splice(0, randomReviewsLength);
+
+    await prisma.review.createMany({
+      data: shuffledReviews.map((review, index) => {
+        return {
+          ...review,
+          restaurantId: restaurant.id,
+          userId: shuffledUsers[index].id,
+        };
+      }),
+    });
+  }
+
+  for (const { id: restaurantId, userId } of restaurants) {
     const ratings = await prisma.review.findMany({
       where: { restaurantId, deletedAt: null },
       select: { rating: true },
@@ -102,20 +86,28 @@ async function main() {
         reviewCount,
       },
     });
-  }
 
-  await prisma.restaurantImage.createMany({
-    data: [
-      {
-        url: 'https://cdn.example.com/pizza.jpg',
-        userId: alice.id,
-      },
-      {
-        url: 'https://cdn.example.com/sushi.jpg',
-        userId: bob.id,
-      },
-    ],
-  });
+    // 80 - 20 percent chance of having image
+    const shouldHaveImage = Math.random() < 0.8;
+
+    if (shouldHaveImage) {
+      const imageId = restaurantId.replaceAll('-', '');
+      const mockCdnUrl = 'https://cdn.example.com/' + imageId + '.jpg';
+      const image = await prisma.restaurantImage.create({
+        data: {
+          url: mockCdnUrl,
+          userId: userId,
+        },
+      });
+
+      await prisma.restaurant.update({
+        where: { id: restaurantId },
+        data: {
+          imageId: image.id,
+        },
+      });
+    }
+  }
 }
 
 main()
